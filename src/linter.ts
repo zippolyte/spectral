2fd4c27b71b525b74b3ae423c98ab3d6020be8b4
Jakub Rożek <jakub@stoplight.io>
2020-04-05 22:58:36 +0200

    refactor(runner): revamp

--- src/linter.ts
+++ src/linter.ts
@@ -1,116 +1,90 @@
 import { decodePointerFragment } from '@stoplight/json';
 import { get } from 'lodash';
 
-import { JsonPath } from '@stoplight/types';
 import { Document } from './document';
-import { DocumentInventory } from './documentInventory';
 import { IMessageVars, message } from './rulesets/message';
 import { getDiagnosticSeverity } from './rulesets/severity';
-import { IFunction, IGivenNode, IRuleResult, IRunRule, IThen } from './types';
+import { IRunningContext } from './runner';
+import { IGivenNode, IRuleResult, IRunRule } from './types';
 import { getClosestJsonPath, getLintTargets, printPath, PrintStyle } from './utils';
-import { IExceptionLocation } from './utils/pivotExceptions';
 
-const arePathsEqual = (one: JsonPath, another: JsonPath): boolean => {
-  if (one.length !== another.length) {
-    return false;
-  }
-
-  for (let i = 0; i < one.length; i++) {
-    if (one[i] !== another[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const isAKnownException = (violation: IRuleResult, locations: IExceptionLocation[]): boolean => {
-  for (const location of locations) {
-    if (violation.source !== location.source) {
-      continue;
-    }
-
-    if (arePathsEqual(violation.path, location.path)) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-// TODO(SO-23): unit test but mock whatShouldBeLinted
 export const lintNode = async (
+  context: IRunningContext,
   node: IGivenNode,
   rule: IRunRule,
-  then: IThen<string, any>,
-  apply: IFunction,
-  inventory: DocumentInventory,
-  exceptionLocations: IExceptionLocation[] | undefined,
-): Promise<IRuleResult[]> => {
-  const givenPath = node.path[0] === '$' ? node.path.slice(1) : node.path;
-  const targets = getLintTargets(node.value, then.field);
-  const results: IRuleResult[] = [];
+  results: Array<Promise<Array<void | IRuleResult[]>>>,
+) => {
+  for (const then of Array.isArray(rule.then) ? rule.then : [rule.then]) {
+    const func = context.functions[then.function];
+    if (typeof func !== 'function') {
+      throw new ReferenceError(`Function ${then.function} not found. Called by rule ${rule.name}.`);
+    }
 
-  for (const target of targets) {
-    const targetPath = givenPath.concat(target.path);
-
-    const targetResults =
-      (await apply(
-        target.value,
-        then.functionOptions || {},
-        {
-          given: givenPath,
-          target: targetPath,
-        },
-        {
-          original: node.value,
-          given: node.value,
-          documentInventory: inventory,
-        },
-      )) || [];
+    const givenPath = node.path[0] === '$' ? node.path.slice(1) : node.path;
+    const targets = getLintTargets(node.value, then.field);
 
     results.push(
-      ...targetResults.map<IRuleResult>(result => {
-        const escapedJsonPath = (result.path || targetPath).map(segment => decodePointerFragment(String(segment)));
-        const associatedItem = inventory.findAssociatedItemForPath(escapedJsonPath, rule.resolved !== false);
-        const path = associatedItem?.path || getClosestJsonPath(inventory.resolved, escapedJsonPath);
-        const document = associatedItem?.document || inventory.document;
-        const range = document.getRangeForJsonPath(path, true) || Document.DEFAULT_RANGE;
-        const value = path.length === 0 ? document.data : get(document.data, path);
-        const source = associatedItem?.document.source;
+      Promise.all(
+        targets.map(async target => {
+          const targetPath = [...givenPath, ...target.path];
+          const targetResults = await func(
+            target.value,
+            then.functionOptions,
+            {
+              given: givenPath,
+              target: targetPath,
+            },
+            {
+              original: node.value,
+              given: node.value,
+              documentInventory: context.documentInventory,
+            },
+          );
 
-        const vars: IMessageVars = {
-          property:
-            associatedItem?.missingPropertyPath && associatedItem.missingPropertyPath.length > path.length
-              ? printPath(associatedItem.missingPropertyPath.slice(path.length - 1), PrintStyle.Dot)
-              : path.length > 0
-              ? path[path.length - 1]
-              : '',
-          error: result.message,
-          path: printPath(path, PrintStyle.EscapedPointer),
-          description: rule.description,
-          value,
-        };
+          return targetResults === void 0
+            ? void 0
+            : targetResults.map<IRuleResult>(result => {
+                const { documentInventory: inventory } = context;
+                const escapedJsonPath = (result.path || targetPath).map(segment =>
+                  decodePointerFragment(String(segment)),
+                );
+                const associatedItem = inventory.findAssociatedItemForPath(escapedJsonPath, rule.resolved !== false);
+                const path = associatedItem?.path || getClosestJsonPath(inventory.resolved, escapedJsonPath);
+                const document = associatedItem?.document || inventory.document;
+                const range = document.getRangeForJsonPath(path, true) || Document.DEFAULT_RANGE;
+                const value = path.length === 0 ? document.data : get(document.data, path);
+                const source = associatedItem?.document.source;
 
-        const resultMessage = message(result.message, vars);
-        vars.error = resultMessage;
+                const vars: IMessageVars = {
+                  property:
+                    associatedItem?.missingPropertyPath && associatedItem.missingPropertyPath.length > path.length
+                      ? printPath(associatedItem.missingPropertyPath.slice(path.length - 1), PrintStyle.Dot)
+                      : path.length > 0
+                      ? path[path.length - 1]
+                      : '',
+                  error: result.message,
+                  path: printPath(path, PrintStyle.EscapedPointer),
+                  description: rule.description,
+                  value,
+                };
 
-        return {
-          code: rule.name,
-          message: (rule.message === void 0 ? rule.description ?? resultMessage : message(rule.message, vars)).trim(),
-          path,
-          severity: getDiagnosticSeverity(rule.severity),
-          ...(source !== null && { source }),
-          range,
-        };
-      }),
+                const resultMessage = message(result.message, vars);
+                vars.error = resultMessage;
+
+                return {
+                  code: rule.name,
+                  message: (rule.message === void 0
+                    ? rule.description ?? resultMessage
+                    : message(rule.message, vars)
+                  ).trim(),
+                  path,
+                  severity: getDiagnosticSeverity(rule.severity),
+                  ...(source !== null && { source }),
+                  range,
+                };
+              });
+        }),
+      ),
     );
   }
-
-  if (exceptionLocations === undefined) {
-    return results;
-  }
-
-  const filtered = results.filter(r => !isAKnownException(r, exceptionLocations));
-  return filtered;
 };
--- src/runner.ts
+++ src/runner.ts
@@ -5,11 +5,40 @@ import { STDIN } from './document';
 import { DocumentInventory } from './documentInventory';
 import { lintNode } from './linter';
 import { getDiagnosticSeverity } from './rulesets/severity';
-import { FunctionCollection, IGivenNode, IRule, IRuleResult, IRunRule, RunRuleCollection } from './types';
+import { FunctionCollection, IRule, IRuleResult, IRunRule, RunRuleCollection } from './types';
 import { RulesetExceptionCollection } from './types/ruleset';
 import { hasIntersectingElement } from './utils/';
 import { generateDocumentWideResult } from './utils/generateDocumentWideResult';
-import { IExceptionLocation, pivotExceptions } from './utils/pivotExceptions';
+// import { pivotExceptions } from './utils/pivotExceptions';
+// import { IExceptionLocation } from './utils/pivotExceptions';
+
+// const arePathsEqual = (one: JsonPath, another: JsonPath): boolean => {
+//   if (one.length !== another.length) {
+//     return false;
+//   }
+//
+//   for (let i = 0; i < one.length; i++) {
+//     if (one[i] !== another[i]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// };
+//
+// const isAKnownException = (violation: IRuleResult, locations: IExceptionLocation[]): boolean => {
+//   for (const location of locations) {
+//     if (violation.source !== location.source) {
+//       continue;
+//     }
+//
+//     if (arePathsEqual(violation.path, location.path)) {
+//       return true;
+//     }
+//   }
+//
+//   return false;
+// };
 
 export const isRuleEnabled = (rule: IRule) => rule.severity !== void 0 && getDiagnosticSeverity(rule.severity) !== -1;
 
@@ -26,27 +55,26 @@ const generateDefinedExceptionsButStdIn = (documentInventory: DocumentInventory)
   );
 };
 
-export const runRules = async (
-  documentInventory: DocumentInventory,
-  rules: RunRuleCollection,
-  functions: FunctionCollection,
-  exceptions: RulesetExceptionCollection,
-): Promise<IRuleResult[]> => {
-  const results: IRuleResult[] = [];
+export interface IRunningContext {
+  documentInventory: DocumentInventory;
+  rules: RunRuleCollection;
+  functions: FunctionCollection;
+  exceptions: RulesetExceptionCollection;
+}
+
+export const runRules = async (context: IRunningContext): Promise<IRuleResult[]> => {
+  const { documentInventory, rules, exceptions } = context;
+
+  const results: Array<Promise<Array<void | IRuleResult[]>>> = [];
 
   const isStdIn = isStdInSource(documentInventory);
 
   if (isStdIn && Object.keys(exceptions).length > 0) {
-    results.push(generateDefinedExceptionsButStdIn(documentInventory));
+    void generateDefinedExceptionsButStdIn(documentInventory);
   }
 
-  const exceptRuleByLocations = isStdIn ? {} : pivotExceptions(exceptions, rules);
-
-  for (const name in rules) {
-    if (!rules.hasOwnProperty(name)) continue;
-
-    const rule = rules[name];
-    if (!rule) continue;
+  for (const rule of Object.values(rules)) {
+    if (!isRuleEnabled(rule)) continue;
 
     if (
       rule.formats !== void 0 &&
@@ -56,50 +84,38 @@ export const runRules = async (
       continue;
     }
 
-    if (!isRuleEnabled(rule)) {
-      continue;
-    }
-
-    let ruleResults: IRuleResult[] = [];
-
-    try {
-      ruleResults = await runRule(documentInventory, rule, functions, exceptRuleByLocations[name]);
-    } catch (e) {
-      console.error(`Unable to run rule '${name}':\n${e}`);
-    }
-
-    results.push(...ruleResults);
+    runRule(context, rule, results);
   }
 
-  return results;
+  return (await Promise.allSettled(results))
+    .flatMap(result => {
+      if (result.status === 'fulfilled') {
+        return result.value.filter(Boolean);
+      }
+
+      return null;
+    })
+    .flat();
 };
 
-const runRule = async (
-  resolved: DocumentInventory,
+const runRule = (
+  context: IRunningContext,
   rule: IRunRule,
-  functions: FunctionCollection,
-  exceptionLocations: IExceptionLocation[] | undefined,
-): Promise<IRuleResult[]> => {
-  const target = rule.resolved === false ? resolved.unresolved : resolved.resolved;
-
-  const results: IRuleResult[] = [];
-  const promises: Array<Promise<void>> = [];
+  results: Array<Promise<Array<void | IRuleResult[]>>>,
+): void => {
+  const target = rule.resolved === false ? context.documentInventory.unresolved : context.documentInventory.resolved;
 
   for (const given of Array.isArray(rule.given) ? rule.given : [rule.given]) {
     // don't have to spend time running jsonpath if given is $ - can just use the root object
     if (given === '$') {
-      promises.push(
-        lint(
-          {
-            path: ['$'],
-            value: target,
-          },
-          resolved,
-          rule,
-          functions,
-          exceptionLocations,
-          results,
-        ),
+      void lintNode(
+        context,
+        {
+          path: ['$'],
+          value: target,
+        },
+        rule,
+        results,
       );
     } else {
       JSONPath({
@@ -107,51 +123,17 @@ const runRule = async (
         json: target,
         resultType: 'all',
         callback: (result: any) => {
-          promises.push(
-            lint(
-              {
-                path: JSONPath.toPathArray(result.path),
-                value: result.value,
-              },
-              resolved,
-              rule,
-              functions,
-              exceptionLocations,
-              results,
-            ),
+          void lintNode(
+            context,
+            {
+              path: JSONPath.toPathArray(result.path),
+              value: result.value,
+            },
+            rule,
+            results,
           );
         },
       });
     }
   }
-
-  await Promise.all(promises);
-  return results;
 };
-
-async function lint(
-  node: IGivenNode,
-  resolved: DocumentInventory,
-  rule: IRunRule,
-  functions: FunctionCollection,
-  exceptionLocations: IExceptionLocation[] | undefined,
-  results: IRuleResult[],
-): Promise<void> {
-  try {
-    for (const then of Array.isArray(rule.then) ? rule.then : [rule.then]) {
-      const func = functions[then.function];
-      if (!func) {
-        console.warn(`Function ${then.function} not found. Called by rule ${rule.name}.`);
-        continue;
-      }
-
-      const validationResults = await lintNode(node, rule, then, func, resolved, exceptionLocations);
-
-      if (validationResults.length > 0) {
-        results.push(...validationResults);
-      }
-    }
-  } catch (e) {
-    console.warn(`Encountered error when running rule '${rule.name}' on node at path '${node.path}':\n${e}`);
-  }
-}
